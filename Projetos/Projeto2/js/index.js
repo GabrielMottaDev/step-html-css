f3f10a779935ac3451c98edd13167799e9cc560f
Gabriel Motta <gabrielmottadev@gmail.com>
2019-01-09 20:45:55 -0200

    Update

--- Projetos/Projeto2/js/index.js
+++ Projetos/Projeto2/js/index.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     var header = $("header");
+    var arrow = $(".arrow-up");
   
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
         if (scroll >= 1) {
-            header.addClass("scrolled");
+            header.addClass("scrolled-header");
+            arrow.addClass("scrolled-arrow-up");
         } else {
-            header.removeClass("scrolled");
+            header.removeClass("scrolled-header");
+            arrow.removeClass("scrolled-arrow-up");
         }
     });
 });
